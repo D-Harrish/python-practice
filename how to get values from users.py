41b9ee7e3363211868b56f7afbf31b708fda25b8
D-Harrish <107384472+D-Harrish@users.noreply.github.com>
2022-06-18 12:08:38 +0530

    Add files via upload

---
+++ how to get values from users.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the value of a:"))
+print(a)
+name=input("Enter your name:")
+print(name)
+b=float(input("Enter the value of b:"))
+print(b)
+c=bool(input("Enter the bool value:"))
+print(c)
